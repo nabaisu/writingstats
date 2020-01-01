931f4a0d09eb76e146f0adc28dd35838b7b01192
Joao Nabais <nabaisaomar@gmail.com>
2020-01-01 20:58:36 +0100

    now makes markdown files & checks for SelfControl

--- writingHabit/pomodoros-escrita.py
+++ writingHabit/pomodoros-escrita.py
@@ -5,7 +5,7 @@ from os import path
 i = 1
 oioioi = os.path.dirname(os.path.abspath(__file__))+"/writings/"
 checki = time.strftime("%Y-%m-%d")+"-reflexoes-" + str(i) + "o-pomodoro-"
-filename = time.strftime("%Y-%m-%d")+"-reflexoes-" + str(i) + "o-pomodoro-"+ ".txt"
+filename = time.strftime("%Y-%m-%d")+"-reflexoes-" + str(i) + "o-pomodoro-"+ ".md"
 adeus = glob.glob(oioioi+checki+ '**')
 if path.exists("/Applications/WriteRoom.app"):
     application = "/Applications/WriteRoom.app"
@@ -37,7 +37,7 @@ else:
     while len(glob.glob(oioioi+time.strftime("%Y-%m-%d")+"-reflexoes-" + str(i) + "o-pomodoro-"+ '**')) != 0:
         print '['+'\033[91m\33[1m'+'-'+'\33[0m'+'] - File '+str(i)+' already existed'
         i += 1
-        filename = time.strftime("%Y-%m-%d")+"-reflexoes-" + str(i) + "o-pomodoro-"+ ".txt"
+        filename = time.strftime("%Y-%m-%d")+"-reflexoes-" + str(i) + "o-pomodoro-"+ ".md"
         print '['+'\033[91m\33[1m'+'-'+'\33[0m'+'] - Creating new file with index - '+str(i)
     f = open(os.path.expanduser(oioioi+filename), "w")
     print '[*] - Creating file: ' + filename
@@ -46,7 +46,7 @@ else:
     bash_command1 = "open  -a "+application+" "+oioioi+filename
     print '[*] - Opening File ...'
     subprocess.call(bash_command1,shell=True)
-    if path.exists("/Applications/WriteRoom.app") and path.exists(os.path.dirname(os.path.abspath(__file__))+"/self-c.applescript"):
-        bash_command2 = "osascript self-c.applescript 30"
+    if path.exists("/Applications/SelfControl.app") and path.exists(os.path.dirname(os.path.abspath(__file__))+"/self-c.applescript"):
+        bash_command2 = "osascript "+os.path.dirname(os.path.abspath(__file__))+"/self-c.applescript 30"
         print '[*] - Closing Down the Internet... See you in 30 minutes'
         subprocess.call(bash_command2,shell=True)
